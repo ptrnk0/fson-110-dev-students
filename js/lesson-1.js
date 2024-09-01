6f626f5defa5aa65e41b6eae00c4ed3646e124ef
olexiy-kireyko <a.kireyko@gmail.com>
2024-09-01 11:13:23 +0300

    second commit

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -14,3 +14,22 @@ const userText = "bcde";
 
 const result = userText.toLowerCase().startsWith("a") ? "yes" : "no";
 console.log(result);
+
+//TODO: 2 ===================================
+// Якщо ваше ім’я містить більше 4 букв і прізвище більше 5, то виведіть на екран число, яке дорівнює сумі букв імені і прізвища.
+// Якщо умова не виконується - вивести “неможливо виконати умову”.
+// Наприклад: “Віктор Сташко” Віктор - 6 літер, Сташко - 6 літер. Виводимо 12.
+// Використовувати вбудований метод length.
+const firstName = "Вік";
+const lastName = "Сташко";
+const firstNameLength = firstName.length;
+const lastNameLength = lastName.length;
+// if (firstNameLength > 4 && lastNameLength > 5) {
+//   console.log(firstNameLength + lastNameLength);
+// } else {
+//   console.log("неможливо виконати умову");
+// }
+
+firstNameLength > 4 && lastNameLength > 5
+  ? console.log(firstNameLength + lastNameLength)
+  : console.log("неможливо виконати умову");
