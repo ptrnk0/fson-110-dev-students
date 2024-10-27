514697508570dc34e343d5ca67e38deba3df052b
1ster14ka <danyazybastik@gmail.com>
2024-10-27 13:07:00 +0200

    lesson-5

--- js/lesson-5.js
+++ js/lesson-5.js
@@ -11,6 +11,7 @@ import { refs } from "./refs.js";
 // Використовуй local storage для зберігання вибраної теми (dark / light)
 refs.checkbox.addEventListener("click", onCheckBoxChange);
 document.addEventListener("DOMContentLoaded", renderPage);
+document.addEventListener("DOMContentLoaded", renderCalendar);
 
 // light theme is default, that's why themeSwitcher is unchecked
 // themeSwitcher.checked = false;
@@ -38,3 +39,90 @@ function renderPage() {
     refs.body.classList.replace("dark", "light");
   }
 }
+
+// console.log(new Date("2001-01-01"));
+
+// console.log(new Date().getDay());
+// console.log(Date.now());
+
+// setInterval(() => {
+//   console.log(new Date().getHours());
+//   console.log(new Date().getMinutes());
+//   console.log(new Date().getSeconds());
+// }, 1000);
+
+const clientDate = new Date("2024-01-01");
+const currentDate = new Date();
+
+const deff = currentDate - clientDate;
+
+function renderCalendar() {
+  const currentDate = new Date();
+  const month = currentDate.toLocaleDateString("uk-UA", { month: "long" });
+  const year = currentDate.toLocaleDateString("uk-UA", { year: "numeric" });
+  const weekDay = currentDate.toLocaleDateString("uk-UA", { weekday: "long" });
+  const day = currentDate.toLocaleDateString("uk-UA", { day: "numeric" });
+
+  refs.calendarContainer.querySelector(".js-month").textContent = month;
+  refs.calendarContainer.querySelector(".js-day-number").textContent = day;
+  refs.calendarContainer.querySelector(".js-day").textContent = weekDay;
+  refs.calendarContainer.querySelector(".js-year").textContent = year;
+}
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(deff)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+
+// setTimeout(() => {
+//   console.log("1");
+// }, 1000);
+
+// console.log("2");
+
+// setTimeout(() => {
+//   console.log("3");
+// }, 0);
+
+// console.log("4");
+
+/**
+ * Перероби функцію на проміс таким чином, щоб проміс повертав значення
+ * через 2 секунди після виклику функції
+ */
+
+// function greet() {
+//   return "hello world";
+// }
+
+// console.log(greet());
+
+// function greet() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("hello world");
+//     }, 2000);
+//   });
+// }
+
+// greet()
+//   .then((response) => console.log(response))
+//   .catch((error) => console.log(error))
+//   .finally(() => {
+//     console.log("finally");
+//   });
